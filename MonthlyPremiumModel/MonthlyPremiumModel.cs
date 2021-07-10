81bde5a9d07cabeed7960b1201a5ebfbcc686ca8
ChungLee Ochieze <chunglee.ochieze@yahoo.co.uk>
2021-07-10 22:16:59 +1000

    frontend form submission

---
+++ MonthlyPremiumModel/MonthlyPremiumModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonthlyPremiumModel
+{
+    public class MonthlyPremiumModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string Occupation { get; set; }
+
+        [Required]
+        public decimal CoverAmount { get; set; }
+    }
+}
